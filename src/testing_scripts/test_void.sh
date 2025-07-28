06c094007036af78762a27f4155c8d9ed148d149
PingpingX98 <18786681463@163.com>
2025-07-28 08:57:45 +0800

    sh_0

--- src/testing_scripts/test_void.sh
+++ src/testing_scripts/test_void.sh
@@ -8,12 +8,12 @@ ckpt=/home/descfly/Projects/NLSPN_ECCV20-master/results/NLSPN_NYU.pt
 for sample in 1500 500 150
 do
   python main.py --dir_data /home/descfly/data/void_release/void_${sample} --data_name VOID \
-    --gpus 0 --max_depth 5.0 \
-    --GRU_iters $GRU_iters --optim_layer_input_clamp $optim_layer_input_clamp --depth_activation_format $depth_activation_format \
-    --test_only --test_augment $test_augment --pretrain $ckpt \
-    --log_dir ../experiments/ \
-    --save "test_void${sample}" \
-    --save_result_only
+    --gpus 0 --max_depth 10.0 --num_sample $sample \
+    --test_only --pretrain $ckpt \
+    --log_dir /data/compare/metric/NLSPN/experiments/ \
+    --preserve_input --batch_size 1 \
+    --save "test_nyu_sample${sample}_inference" \
+    --legacy \
 done
 
 
