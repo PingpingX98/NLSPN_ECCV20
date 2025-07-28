c34598efe4270f8089b8db43aeb8f3b7c5f49c43
PingpingX98 <18786681463@163.com>
2025-07-28 16:59:41 +0800

    sh

--- src/testing_scripts/test_kitti.sh
+++ src/testing_scripts/test_kitti.sh
@@ -1,11 +1,11 @@
 
 ckpt=/home/descfly/Projects/NLSPN_ECCV20-master/results/NLSPN_KITTI_DC.pt
 
-for lidar_lines in 8
+for lidar_lines in 64
 do
-  python main.py --dir_data /data/kitti_depth \
+  python main_time.py --dir_data /data/kitti_depth \
   --data_name KITTIDC --split_json ../data_json/kitti_dc.json \
-  --patch_height 240 --patch_width 1216 --gpus 0 --max_depth 90.0 --top_crop 100\
+  --patch_height 352 --patch_width 1216 --gpus 0 --max_depth 90.0 --top_crop 0\
   --num_sample 0 --test_only --pretrain $ckpt \
   --preserve_input --legacy  \
   --lidar_lines $lidar_lines \
