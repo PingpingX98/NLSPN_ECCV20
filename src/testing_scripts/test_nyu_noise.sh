1c4c5af217b0978fcc6dd48af9ea2c428bb4a7a1
PingpingX98 <18786681463@163.com>
2025-07-29 17:08:57 +0800

    nyu noise1

--- src/testing_scripts/test_nyu_noise.sh
+++ src/testing_scripts/test_nyu_noise.sh
@@ -6,17 +6,20 @@ ckpt=/home/descfly/Projects/NLSPN_ECCV20-master/results/NLSPN_NYU.pt
 # for sample in 300 400
 for noise_type in gaussian impulse rayleigh gamma exponential uniform
 do
-python main.py --dir_data /home/descfly/data/nyudepthv2 --data_name NYU --split_json ../data_json/nyu.json \
+python main_memory.py --dir_data /home/descfly/data/nyudepthv2 --data_name NYU --split_json ../data_json/nyu.json \
     --gpus 0 --max_depth 10.0 --num_sample 500 \
-    --test_only  --pretrain $ckpt \
+    --test_only --pretrain $ckpt \
     --log_dir /data/compare/metric/NLSPN/experiments/ \
-    --save "test_nyu_${noise_type}" --batch_size 1 \
-    --add_noise --noise_type $noise_type \
-    --save_result_only \
-    --preserve_input \
-    --legacy \    
-    # --save "test_nyu_8msk_sample${sample}" \
+    --preserve_input --batch_size 1 \
+    --save "test_nyu_noise${noise_type}" \
+    --add_noise --noise_type ${noise_type} \
+    --legacy \
+    # --save_result_only  
+    # --save "$test_nyu_sample${sample}" \
     # --save 'nyu_1.10' \
+    # --save_result_only  
+    # --save_full --save_image  
+    # --save_full --save_pointcloud_visualization
 done
 
 # do
